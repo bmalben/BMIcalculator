b3602eaea60d6000e5022173642638223cadb6ab
Your Name <bimalbenny2011@gmail.com>
2024-08-19 01:44:52 +0530

    updated

--- home.js
+++ home.js
@@ -22,6 +22,7 @@ if(!isNaN(bmi))
     {
         if(bmi<18.5){
             y='Underweight'
+            stat2.innerHTML=`<h4 style=color:yellow>${roundedBMI}</h4>`
             stat.innerHTML=`<h4 style=color:yellow>${y}</h4>`
             image.innerHTML=`<img src="./images/skinny.png" alt="">`
             // const utterance = new SpeechSynthesisUtterance(`Hey ${uname}Your BMI is ${roundedBMI} and your status is ${y}. You are Skinny Dude...`);
@@ -29,13 +30,15 @@ if(!isNaN(bmi))
         }
         else if (bmi>=18.5&&bmi<24.9){
             y='Normal weight';
-            stat.innerHTML=`<h4 style=color:green>${y}</h4>`
+            stat2.innerHTML=`<h4 style=color:greenyellow>${roundedBMI}</h4>`
+            stat.innerHTML=`<h4 style=color:greenyellow>${y}</h4>`
             image.innerHTML=`<img src="./images/normal.png" alt="">`
             // const utterance = new SpeechSynthesisUtterance(`Hey ${uname}Your BMI is ${roundedBMI} and your status is ${y}. You are Perfect Dude...`);
             // speechSynthesis.speak(utterance);
         } 
         else if(bmi>=25&&bmi<29.9){
             y='Overweight';
+            stat2.innerHTML=`<h4 style=color:orange>${roundedBMI}</h4>`
             stat.innerHTML=`<h4 style=color:orange>${y}</h4>`
             image.innerHTML=`<img src="./images/overweight.png" alt="">`
             // const utterance = new SpeechSynthesisUtterance(`Hey ${uname}Your BMI is ${roundedBMI} and your status is ${y}. You are Bulk Dude...`);
@@ -43,6 +46,7 @@ if(!isNaN(bmi))
         } 
         else{
             y='Obese';
+            stat2.innerHTML=`<h4 style=color:red>${roundedBMI}</h4>`
             stat.innerHTML=`<h4 style=color:red>${y}</h4>`
             image.innerHTML=`<img src="./images/obese.png" alt="">`
             // const utterance = new SpeechSynthesisUtterance(`Hey ${uname}Your BMI is ${roundedBMI} and your status is ${y}. You are Fat Man...`);
